39172c01edac8b3f430cc7ef7a2f670fa2085eb7
Dan Hart <danhart@notonthehighstreet.com>
2014-08-27 18:13:58 +0100

    Emit single orders via socket.io

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ var io = require('socket.io').listen(10052, {
 });
 
 io.sockets.on('connection', function (socket) {
-    var orderListener = function(orders) {
+    var ordersListener = function(orders) {
         socket.emit('orders', orders);
     };
 
-    var intlOrderListener = function(intl_orders) {
+    var orderListener = function(order) {
+        socket.emit('order', order);
+    };
+
+    var intlOrdersListener = function(intl_orders) {
         socket.emit('intl_orders', intl_orders);
     };
 
-    ordersService.on('orders', orderListener);
-    ordersService.on('intl_orders', intlOrderListener);
+    var intlOrderListener = function(intl_order) {
+        socket.emit('intl_order', intl_order);
+    };
+
+    ordersService.on('orders', ordersListener);
+    ordersService.on('order', orderListener);
+    ordersService.on('intl_orders', intlOrdersListener);
+    ordersService.on('intl_order', intlOrderListener);
 
     socket.on('disconnect', function () {
-        ordersService.removeListener('orders', orderListener);
-        ordersService.removeListener('intl_orders', intlOrderListener);
+        ordersService.removeListener('orders', ordersListener);
+        ordersService.removeListener('order', orderListener);
+        ordersService.removeListener('intl_orders', intlOrdersListener);
+        ordersService.removeListener('intl_order', intlOrderListener);
     });
 });
